5a8afd120c296efe6339c2cd2bfdc08abdbea3e2
barolina <kittenlora@yandex.ru>
2018-04-11 00:49:44 +0300

    test

--- project/main.py
+++ project/main.py
@@ -5,13 +5,9 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.NOTSET)
 
 
-def fast_iter(context, func, *args, **kwargs):
-    """ call func on each xml element chosen when create context
-
-    context = etree.iterparse('path to file', tag='your tag', events)
-    """
+def fast_iter(context, func):
     for event, elem in context:
-        func(elem, *args, *kwargs)
+        func(elem)
         elem.clear()
         while elem.getprevious() is not None:
             del elem.getparent()[0]
@@ -30,14 +26,23 @@ def render_template(content, tpl):
 
 
 def client_text(element):
+    pass
+
+def node_to_dict(node):
+    pass
+
+
+class TitleDict(list):
+    def __init__(self, node):
+        pass
 
 
 def process_element(element):
-    _reason = element.xpath('//Reason/text()')
-    _purpose = element.xpath('//Purpose/text()')
-    context  = {
-        'reason' :
-    }
+    # _reason = element.xpath('//Reason/text()')
+    # print(type(_reason))
+    # _purpose = list(element.xpath('//Purpose/text()'))
+    # print(_purpose)
+
     # context = {
     #     'reason ': ['fruit', 'vegetable', 'stone', 'thing'],
     #     'tbl_contents': [
@@ -46,15 +51,18 @@ def process_element(element):
     #         {'label': 'green', 'cols': ['guava', 'cucumber', 'aventurine', 'card']},
     #     ]
     # }
-    _el = element.xpath('//Contractor/FamilyName1/text()')
-    print(_el)
+    _entity_spatial =element.xpath('//NewParcel/child::EntitySpatial/child::*/child::*/*[contains(name(),"Ordinate")]/@*')
+    print(_entity_spatial)
+    # _el = list(filter(lambda x: x != '\n', element.xpath('//NewParcel/*/text()')))
+    # print(_el)
     # print(element.xpath('//Contractor/FamilyName'))
     # for node in element:  # Перебираем элементы
     #     print(node.tag, node.keys(), node.values())
 
 
 def start(path):
-    context = etree.iterparse(path, events=('start', 'end',),tag='GeneralCadastralWorks')
+    context = etree.iterparse(path, events=('start', 'end',),tag='FormParcels')
+    # process_element(context)
     fast_iter(context, process_element)
 
 
