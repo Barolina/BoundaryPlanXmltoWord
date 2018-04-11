35905628c00c9132a60e1c80e04202e26e8c27bb
barolina <kittenlora@yandex.ru>
2018-04-12 00:00:30 +0300

    update title and input_data

--- project/main.py
+++ project/main.py
@@ -3,17 +3,22 @@ from lxml import etree
 import io
 import config as cnfg
 from docxtpl import DocxTemplate
+import memory_profiler
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.NOTSET)
 
-
-def fast_iter(context, func, **kwargs):
+def fast_iter(context, func, args=[], kwargs={}):
+    # http://www.ibm.com/developerworks/xml/library/x-hiperfparse/
+    # Author: Liza Daly
+    print(f"context {context}")
     for event, elem in context:
-        func(elem)
+        print(elem)
+        func(elem, *args, **kwargs)
         elem.clear()
         while elem.getprevious() is not None:
             del elem.getparent()[0]
+    print("del ")
     del context
 
 def extract_file(path_zip):
@@ -43,23 +48,28 @@ def get_children_text(node, *args):
     return res
 
 
-def get_children_dict(node, *args):
-    res = dict()
-    for el in node:
-        res[el.tag] = el.text
-    return res
 
 
-class Title:
+class XmlTitleDict:
 
     def __init__(self, node):
-        self.node  = node
+        self.node = node
+        self.contractor = None
 
-    def node_contractor(self):
-        return get_children_dict(self.node.xpath('Contractor/child::*'))
+    def _children_dict(self, node, *args):
+        res = dict()
+        for el in node:
+            res[el.tag] = el.text
+        return res
+
+    def setcontractor(self):
+        self.contractor= self._children_dict(self.node.xpath('Contractor/child::*'))
+
+    def getcontractor(self):
+        return self.contractor
 
     def reason(self):
-        return  ''.join(self.node.xpath('Reason/text()'))
+        return ''.join(self.node.xpath('Reason/text()'))
 
     def purpose(self):
         return ''.join(self.node.xpath('Purpose/text()'))
@@ -68,17 +78,26 @@ class Title:
         return ' '.join(self.node.xpath('Clients/*[1]/child::*/node()/text()'))
 
     def contrsactor_fio(self):
-        return f"{self.node_contractor().get('FamilyName', '')} {self.node_contractor().get('FirstName', '')} " \
-               f"{self.node_contractor().get('Patronymic', '')}"
+        if not self.contractor:
+            self.setcontractor()
+        return f"{self.contractor.get('FamilyName', '')} " \
+               f"{self.contractor.get('FirstName', '')} " \
+               f"{self.contractor.get('Patronymic', '')}"
 
     def ncertificate(self):
-        return self.node_contractor().get('NCertificate', '')
+        if not self.contractor:
+            self.setcontractor()
+        return self.contractor.get('NCertificate', '')
 
     def telefon(self):
-        return self.node_contractor().get('Telephone', '')
+        if not self.contractor:
+            self.setcontractor()
+        return self.contractor.get('Telephone', '')
 
     def email(self):
-        return self.node_contractor().get('Email', '')
+        if not self.contractor:
+            self.setcontractor()
+        return self.contractor.get('Email', '')
 
     def organization(self):
         return ' '.join(self.node.xpath('Contractor/Organization/node()/text()'))
@@ -86,57 +105,76 @@ class Title:
     def data(self):
         return ' '.join(self.node.xpath('@DateCadastral'))
 
+    def to_dict(self):
+        value_title = [self.reason(),self.purpose(), self.client(),\
+                       self.contrsactor_fio(), self.ncertificate(), self.telefon(),\
+                       self.email(),self.organization(), self.data()]
+        return dict(zip(cnfg.TITLE_KEY, value_title))
 
-def title_to_context_tpl(node, path_tpl, file_res):
+
+class XmlInputDataDict:
+    NAME = 'Name'
+    NUMBER = 'Number'
+    DATE = 'Date'
+    pathListDocuments = 'Documents/child::*/child::*'
+
+    def __init__(self, node):
+        self.node = node
+
+    def name_doc(self):
+        pass
+
+    def to_dict(self):
+        el = self.node.xpath(self.pathListDocuments)
+        print(etree.tostringlist(el))
+        name_list = []
+        note_list = []
+        for _ in el:
+            if _.tag == self.NAME:
+                name_list.append(_.text)
+            elif _.tag in [self.NUMBER, self.DATE]:
+                note_list.append(_.text)
+        # del el
+        return dict(zip(name_list, cnfg.INPUT_DATA_KEY))
+        # for _ in el:
+        #     for attrib_name in _.attrib:
+        #         print('@' + attrib_name + '=' + _.attrib[attrib_name])
+        #         _name.append(_.attrib)
+        #     subfields = _.getchildren()
+        #     for subfield in subfields:
+        #         print('subfield=' + subfield.text)
+        # print(_name)
+
+
+def title_to_context_tpl(node,name_class, path_tpl, file_res):
     """
     :param node: узел GeneralCadastralWorks
     :return: dict
     """
     if len(node) > 0:
         tpl = DocxTemplate(path_tpl)
-        title = Title(node)
-        context= dict()
-        context['col'] = 'test--reet'
-        context['REASON']  = title.reason()
-        context[cnfg.PURPOSE] = title.purpose()
-        context[cnfg.CLIENT] = title.client()
-        context[cnfg.FIO] = title.contrsactor_fio()
-        context[cnfg.NCERTIFICATE]  = title.ncertificate()
-        context[cnfg.TELEPHONE] = title.telefon()
-        context[cnfg.EMAIL] = title.email()
-        context[cnfg.ORGANIZATION] = title.organization()
-        context[cnfg.MP_DATA] = title.data()
-        tpl.render(context)
-        tpl.save('res/title.docx')
-
+        title = name_class(node)
+        tpl.render(title.to_dict())
+        tpl.save(file_res)
+        print(title.to_dict())
 
 def process_element(element):
-    # _reason = element.xpath('//Reason/text()')
-    # print(type(_reason))
-    # _purpose = list(element.xpath('//Purpose/text()'))
-    # print(_purpose)
-
-    # context = {
-    #     'reason ': ['fruit', 'vegetable', 'stone', 'thing'],
-    #     'tbl_contents': [
-    #         {'label': 'yellow', 'cols': ['banana', 'capsicum', 'pyrite', 'taxi']},
-    #         {'label': 'red', 'cols': ['apple', 'tomato', 'cinnabar', 'doubledecker']},
-    #         {'label': 'green', 'cols': ['guava', 'cucumber', 'aventurine', 'card']},
-    #     ]
-    # }
-    _entity_spatial =element.xpath('//NewParcel/child::EntitySpatial/child::*/child::*/*[contains(name(),"Ordinate")]/@*')
-    print(_entity_spatial)
-    # _el = list(filter(lambda x: x != '\n', element.xpath('//NewParcel/*/text()')))
-    # print(_el)
-    # print(element.xpath('//Contractor/FamilyName'))
+    pass
     # for node in element:  # Перебираем элементы
     #     print(node.tag, node.keys(), node.values())
+
+def new_parcel_content(element):
+    _entity_spatial = element.xpath('//NewParcel/child::EntitySpatial/child::*/child::*/*[contains(name(),"Ordinate")]/@*')
+    print(_entity_spatial)
+    print('es')
 
 
 def start(path):
     # формирование шаблона title.doc
     context = etree.iterparse(path, events=('start', 'end',),tag='GeneralCadastralWorks')
-    fast_iter(context, title_to_context_tpl, 'template/common/title.docx')
+    fast_iter(context, title_to_context_tpl, args=(XmlTitleDict,'template/common/title.docx', 'res/title.docx'))
+    context = etree.iterparse(path, events=('start', 'end',),tag='InputData')
+    fast_iter(context, title_to_context_tpl, args=(XmlInputDataDict, 'template/common/title.docx', 'res/title.docx'))
 
 
 if __name__ == '__main__':
