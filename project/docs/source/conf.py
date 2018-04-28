5a2364d177451a8550075c8d57f68b74219c6e7e
Larisa <solow_larisa@mail.ru>
2018-04-28 10:25:53 +0300

    rst

--- project/docs/source/conf.py
+++ project/docs/source/conf.py
@@ -14,8 +14,9 @@
 #
 import os
 import sys
+sys.path.insert(0, os.path.abspath('../../'))
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'core'))
+# sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'core'))
 
 # -- Project information -----------------------------------------------------
 
