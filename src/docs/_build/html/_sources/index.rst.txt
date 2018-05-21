cd29d27da3871a75e5fbc51c531efa758ec6cd59
larisa <kittenlora@yandex.ru>
2018-05-21 11:17:22 +0300

    docs

--- src/docs/_build/html/_sources/index.rst.txt
+++ src/docs/_build/html/_sources/index.rst.txt
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Описание приложения MP_to_Word (печать межевого плана в Word)!
+Print xml_mp to word
 =====================================
 
 .. toctree::
--- src/docs/conf.py
+++ src/docs/conf.py
@@ -177,3 +177,25 @@ html_theme_options = {
     'includehidden': True,
     'titles_only': False
 }
+
+
+
+# -- Options for Epub output ----------------------------------------------
+
+# Bibliographic Dublin Core info.
+epub_title = project
+epub_author = author
+epub_publisher = author
+epub_copyright = copyright
+
+# The unique identifier of the text. This can be a ISBN number
+# or the project homepage.
+#
+# epub_identifier = ''
+
+# A unique identification for the text.
+#
+# epub_uid = ''
+
+# A list of files that should not be packed into the epub file.
+epub_exclude_files = ['search.html']
