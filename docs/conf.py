099c82598996b3f8d00292290a623762736025bf
Larisa <solow_larisa@mail.ru>
2018-05-22 14:06:33 +0300

    cnfg

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,8 @@ sys.path.insert(0, os.path.abspath('../tests/'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.coverage']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.coverage','sphinx.ext.todo',
+    'sphinx.ext.mathjax',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
